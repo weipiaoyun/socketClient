e393c3442acae8b1039cfc81a26921b1029250f5
weipiaoyun <525921240@qq.com>
2018-12-10 15:58:59 +0800

    测试

--- chatRoomClient/Program.cs
+++ chatRoomClient/Program.cs
@@ -17,6 +17,8 @@ namespace chatRoomClient
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            string tets;
         }
     }
 }
